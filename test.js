24da63ac1d89a1a4ab89d2304e65408a42a53f67
Jayaprakash02 <143706807+Jayaprakash02@users.noreply.github.com>
2024-09-14 21:37:47 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(helmet());
+
+app.get('/', function (req, res) {
+    res.send("hello!!");
+});
+
+app.listen("3000",function(){
+    console.log("server is running on port 3000");
+});
+
